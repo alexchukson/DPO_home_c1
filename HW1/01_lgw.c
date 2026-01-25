d793f7f8b1fe1ca9ba6b9a5f6bd75f152a7378c7
alexchukson <83650504+alexchukson@users.noreply.github.com>
2026-01-25 16:40:50 +0300

    012-1

--- HW1/01_lgw.c
+++ HW1/01_lgw.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 void main(){
-    printf("Hello world!");
     printf(
-"	 Let's\n
-       go\n
+"	 Let's\n\
+       go\n\
          to walk\n");
     exit(0);
 }
--- HW1/02_stars.c
+++ HW1/02_stars.c
@@ -1,11 +1,10 @@
   void main(){
-    printf("Hello world!");
     printf(
-"    *\n
-    ***\n
-   *****\n
-  *******\n
-   HHOHH\n
+"    *\n\
+    ***\n\
+   *****\n\
+  *******\n\
+   HHOHH\n\
    ZZZZZ\n");
     exit(0);
    }  
