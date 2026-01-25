ccaf4231ba47d9c2c9afd48f0f0c10ba5af9479a
alexchukson <83650504+alexchukson@users.noreply.github.com>
2026-01-25 16:25:51 +0300

    012

---
+++ HW1/01_lgw.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(){
+    printf("Hello world!");
+    printf(
+"	 Let's\n
+       go\n
+         to walk\n");
+    exit(0);
+}
---
+++ HW1/02_stars.c
@@ -0,0 +1,11 @@
+  void main(){
+    printf("Hello world!");
+    printf(
+"    *\n
+    ***\n
+   *****\n
+  *******\n
+   HHOHH\n
+   ZZZZZ\n");
+    exit(0);
+   }  
