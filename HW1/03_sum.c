a58f326e8b7fc2ed6cfc24f4977adcee73a0f351
derenchuk <alexchuk@bk.ru>
2026-01-27 10:41:35 +0300

    0-4 4 fixed

--- HW1/03_sum.c
+++ HW1/03_sum.c
@@ -2,8 +2,8 @@
 
 void main(){
     int a = 0, b = 0, c = 0;
-    printf("Enter 3 int numbers: \n");
+    // printf("Enter 3 int numbers: \n");
     scanf("%d%d%d", &a, &b, &c);
-    printf("The sum of three integers = %d \n", a + b + c);
+    printf("%d+%d+%d=%d \n", a, b, c, a + b + c);
     exit(0);
 }
